f319f3abd27ebb117a8dd011acd9a82c0d9704bd
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2023-10-14 17:37:01 +0200

    Updated nvim config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -13,8 +13,8 @@ return {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
+        -- elseif luasnip.expand_or_jumpable() then
+        --   luasnip.expand_or_jump()
         elseif has_words_before() then
           cmp.complete()
         else
