c7827010061073a1ddccdde2b4f01669cb35cd8c
JoakimMellonn <joakim@mellonn.com>
2024-07-09 07:56:45 +0200

    changed config a bit

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,28 +1,29 @@
-return {
-  "nvimtools/none-ls.nvim",
-  opts = function()
-    local nls = require("null-ls")
-
-    nls.builtins.formatting.clang_format.with({
-      extra_args = { "--tab-width", "4" },
-    })
-    nls.builtins.formatting.stylua.with({
-      extra_args = { "--tab-width", "4" },
-    })
-    nls.builtins.formatting.biome.with({
-      extra_args = { "--tab-width", "4" },
-    })
-    nls.builtins.formatting.gofumpt.with({
-      extra_args = { "--tab-width", "4" },
-    })
-    nls.builtins.formatting.goimports.with({
-      extra_args = { "--tab-width", "4" },
-    })
-    nls.builtins.formatting.markdownlint.with({
-      extra_args = { "--tab-width", "4" },
-    })
-    nls.builtins.formatting.shfmt.with({
-      extra_args = { "--tab-width", "4" },
-    })
-  end,
-}
+return {}
+-- return {
+--   "nvimtools/none-ls.nvim",
+--   opts = function()
+--     local nls = require("null-ls")
+--
+--     nls.builtins.formatting.clang_format.with({
+--       extra_args = { "--tab-width", "4" },
+--     })
+--     nls.builtins.formatting.stylua.with({
+--       extra_args = { "--tab-width", "4" },
+--     })
+--     nls.builtins.formatting.biome.with({
+--       extra_args = { "--tab-width", "4" },
+--     })
+--     nls.builtins.formatting.gofumpt.with({
+--       extra_args = { "--tab-width", "4" },
+--     })
+--     nls.builtins.formatting.goimports.with({
+--       extra_args = { "--tab-width", "4" },
+--     })
+--     nls.builtins.formatting.markdownlint.with({
+--       extra_args = { "--tab-width", "4" },
+--     })
+--     nls.builtins.formatting.shfmt.with({
+--       extra_args = { "--tab-width", "4" },
+--     })
+--   end,
+-- }
