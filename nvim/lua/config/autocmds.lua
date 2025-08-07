6f50b4b5a90f95db3df2d208d8980a768d4917b0
JoakimMellonn <joakim@mellonn.com>
2025-08-07 08:09:00 +0200

    no format in ntnx json

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function in_ntnx_auto()
+  local cwd = vim.fn.getcwd()
+  return cwd:match("/home/joped/repo/ntnx")
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype == "json" and in_ntnx_auto() then
+      vim.g.autoformat = false
+      return
+    end
+    vim.g.autoformat = true
+  end,
+})
